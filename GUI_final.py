4a21521689c805cfd2ba218af742e62c2d2285d9
monkeybutfart <jm23050@mbc.school.nz>
2026-07-02 13:50:11 +1200

    I thinks its I hope

--- GUI_final.py
+++ GUI_final.py
@@ -4,12 +4,17 @@ from tkinter import ttk, messagebox
 
 
 #data list
+#curret hires
 full_name = []
 hired_yes = []
 amount_hired = []
 receipt_number = []
 
-
+#all returned hires 
+returned_name = []
+returned_hired = []
+returned_amount = []
+returned_receipt = []
 
 #Constants
 MIN_HIRED = 1
@@ -42,14 +47,34 @@ def log_data():
     name = name_entry.get().strip()
     hired = hired_entry.get().strip()
     amount = amount_hired_entry.get().strip()
+    receipt = receipt_number_entry.get().strip()
     
     
-    #genrates a receipt and stops it from being the same
-    receipt = random.randint(1000000000, 99999999999999999999)
-    while receipt in receipt_number:
-        receipt = random.randint(1000000000, 99999999999999999999)
-    
-            
+    if receipt == "":
+        while True:
+            receipt = str(random.randint(1000000000, 99999999999999999999))
+            if receipt not in receipt_number:
+                break
+
+    # ensure receipt is string (safety step)
+    receipt = str(receipt)
+
+#checks if the the receipt is a number 
+    if not receipt.isdigit():
+        messagebox.showerror("Error", "Receipt must be a number.")
+        return
+
+#checks if the receipt is already used 
+    if receipt in receipt_number:
+        messagebox.showerror("Error", "Receipt already exists.")
+        return
+
+    # update entry box
+    receipt_number_entry.delete(0, tk.END)
+    receipt_number_entry.insert(0, receipt)
+
+
+
 
     #stops the program from stopping to errors
     if name == "" or hired == "":
@@ -66,6 +91,9 @@ def log_data():
     #Stops the amount from being -1
     if valid == -1:
         return
+    
+    
+    
     else:
         print(f"Name: {name}, Hired: {hired}, Amount: {amount}, receipt: {receipt} ")
         messagebox.showinfo(f"Info Logged", f"Name: {name}\nHired: {hired}\nAmount Hired: {amount}\nReceipt: {receipt}")
@@ -76,13 +104,14 @@ def log_data():
         returns_box["values"] = receipt_number
         hired_entry.delete(0, tk.END)
         amount_hired_entry.delete(0, tk.END)
+        receipt_number_entry.delete(0, tk.END)
         return
 
  #saves the data to a text file     
 def save_data():
     
 
-    with open("Party_hire.txt", "w") as file:
+    with open("Curret_Party_hire.txt", "w") as file:
         
         file.write("====================\n")
         file.write("Party hire\n")
@@ -96,24 +125,50 @@ def save_data():
             
             
     return
+
+def save_returns():
+    with open("Returns_Party_hire.txt", "w") as file:
+            file.write("====================\n")
+            file.write("Returned Party Hire\n")
+            file.write("====================\n\n")
+
+            for i in range(len(returned_name)):
+                file.write(f"Name: {returned_name[i]}\n")
+                file.write(f"Hired: {returned_hired[i]}\n")
+                file.write(f"Amount Hired: {returned_amount[i]}\n")
+                file.write(f"Receipt: {returned_receipt[i]}\n\n")
+
+
+
 #allows the user to remove returns from the text file and list       
 def returns():
     remove = returns_box.get()
     
+ 
     if remove == "":
         messagebox.showerror("Error", "select a receipt.")
         return
     
-    receipt = int(remove)
+    receipt = str(remove)
+
+    
 
     #uses the recpeits index to delete the index form the file
+    
     if receipt in receipt_number:
         index = receipt_number.index(receipt)
+        returned_name.append(full_name[index])
+        returned_hired.append(hired_yes[index])
+        returned_amount.append(amount_hired[index])
+        returned_receipt.append(receipt_number[index])
+        index = receipt_number.index(receipt)
+
         del full_name[index]
         del hired_yes[index]
         del amount_hired[index]
         del receipt_number[index]
         
+        save_returns()
         save_data()
         returns_box["values"] = receipt_number
         
@@ -146,17 +201,21 @@ ttk.Label(root, text="Amount Hired: "). grid(row=3, column=0, sticky="e")
 amount_hired_entry= ttk.Entry(root, width=25)
 amount_hired_entry.grid(row=3, column =1)
 
+tk.Label(root, text="receipt: "). grid(row=5, column=0, sticky="e")
+receipt_number_entry= ttk.Entry(root, width=25)
+receipt_number_entry.grid(row=5, column =1)
+
 submit_btn = ttk.Button(root, text= "Save", command=lambda:(log_data(), save_data()))
-submit_btn.grid(row=5, column=1, pady=10)
+submit_btn.grid(row=6, column=1, pady=10)
 
 title_label = ttk.Label(root, text="Returns", font=("Verdana", 18, "bold"))
-title_label.grid(row=6, column=0, columnspan=2, pady=10)
+title_label.grid(row=7, column=0, columnspan=2, pady=10)
 
-ttk.Label(root, text= "Put in the receipt number: "). grid(row=7, column=0, sticky="e")
+ttk.Label(root, text= "Put in the receipt number: "). grid(row=8, column=0, sticky="e")
 returns_box = ttk.Combobox(root, values = [], state = "readonly")
-returns_box.grid(row=7, column=1)
+returns_box.grid(row=8, column=1)
 
-submit_btn = ttk.Button(root, text= "remove return",command= returns)
-submit_btn.grid(row=8, column=1, pady=10)
+submit_btn = ttk.Button(root, text= "return",command= returns)
+submit_btn.grid(row=9, column=1, pady=10)
 
 root.mainloop() 
